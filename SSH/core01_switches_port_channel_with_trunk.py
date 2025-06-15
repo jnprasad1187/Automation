23cedc28ad95036e0d4b19251def7fafbcad862b
jnprasad1187 <joseph.prasad@gmail.com>
2025-06-14 21:33:11 -0700

    first commit

---
+++ SSH/core01_switches_port_channel_with_trunk.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+
+CORE_SW01 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.86.243',
+    'username': 'cisco',
+    'password': 'cisco',
+}
+
+with open('core01_switches_port_channel_with_trunk') as f:
+    lines = f.read().splitlines()
+print (lines)
+
+all_devices = [CORE_SW01]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print (output)
---
+++ SSH/core02_switches_port_channel_with_trunk.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+
+CORE_SW02 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.86.244',
+    'username': 'cisco',
+    'password': 'cisco',
+}
+
+with open('core02_switches_port_channel_with_trunk') as f:
+    lines = f.read().splitlines()
+print (lines)
+
+all_devices = [CORE_SW02]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print (output)
